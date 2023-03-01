d0292beaf722d1fb0a6e6dc5b63ecf75a0172bd4
jod431047 <jod431047@gmail.com>
2023-03-01 22:34:43 +0100

    python

---
+++ index.py
@@ -0,0 +1,49 @@
+'''
+x = 'python'
+for letter in x :
+    print(letter)
+'''
+
+
+
+""" for x in range(2,21,2):
+      print(x)
+
+for y in range(3,33,3):
+      print(y) """
+
+""" for j in range(4,44,4):
+    print(j)
+for z in range(1,6):
+    for j in range(1,13):
+        print(f"{j} x {z} = {j*z}") """
+
+""" d =[[1,2,3],[4,5,6]]
+for x in d:
+    for y in x :
+        print(y) """
+""" for x in range(50):
+    if x == 20:
+        continue
+    print(x)        
+ """
+""" print('Number\tSquare')
+print('---------------------')
+for x in range(1,11):
+    print(x,'\t',x*x) """
+""" rows = int(input('Enter rows :'))
+cols = int(input('Enter cols :'))
+for x in range(rows):
+    for y in range(cols):
+        print('*',end='')
+    print(' ')
+ """
+""" for x in range(1,9):
+    for y in range(x):
+        print('*',end='')
+    print(' ')
+
+
+for x in range(1,9):
+    print('*'*x) """
+
