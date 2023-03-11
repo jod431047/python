b9dbe7bf107685c2883bd81698d7dc2f43520658
jod431047 <jod431047@gmail.com>
2023-03-11 18:46:19 +0100

    python

---
+++ index1.py
@@ -0,0 +1,45 @@
+#string
+x ='jihadbd\nlazize'
+z ='abdlrahman'
+print(x)
+#print(z)
+
+l = "jihad abdlazize"
+print(l[-2])
+
+h = "jihad abdlazize"
+print(h[11:10])
+
+
+name = "karam"
+print(name.islower())
+
+
+name2 ="KARAM"
+print(name2.isupper())
+
+name3="KARAM"
+print(name3.capitalize())
+
+name4="KARAM"
+print(name4.title())
+
+programe_lang = "html css javascript php python"
+print(programe_lang.split())
+
+programe_lang1 = "html_ css_ javascript_ php_ python"
+print(programe_lang1.split())
+
+programe_lang2 = "html_ css_ javascript_ php_ python"
+print(programe_lang2.split("h"))
+
+programe_lang3 = "html_ css_ javascript_ php_ python"
+print(programe_lang3.split("_",3))
+
+f = "html css js php python"
+print(len(f))
+
+d = "html css js php python"
+print(f.replace("p","d"))
+
+
