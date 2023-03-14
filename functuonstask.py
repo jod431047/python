928556ee5487799994133b5d92f5d5c96a70ae41
jod431047 <jod431047@gmail.com>
2023-03-14 22:31:07 +0100

    python

---
+++ functuonstask.py
@@ -0,0 +1,8 @@
+
+
+def firstfunc(x=5, y=10):
+    return print(f'x = {x} , y = {y}')
+firstfunc(5 , 10)
+                                         #x = 5 , y = 10
+firstfunc(5.6 ,10)                       #x = 5.6 , y = 10
+
