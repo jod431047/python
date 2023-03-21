0b1c1441a0e1c5c1acf90383483464f98a2f239b
jod431047 <jod431047@gmail.com>
2023-03-21 21:37:51 +0100

    python

---
+++ project.py
@@ -0,0 +1,17 @@
+class calc:
+    def sum(self,x,y):
+        print(x,y)
+
+    def mul(self,x,y):
+        print(x*y)
+
+ #constructor
+    def __int__(self,name):
+        print(f'welcome {name}')
+
+f =calc('welcome')
+
+
+
+
+
