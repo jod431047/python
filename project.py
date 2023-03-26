a30b952535c24cf6cbec76b57006b6753e72fa1f
jod431047 <jod431047@gmail.com>
2023-03-26 22:50:49 +0200

    python

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+'''
 class Calc:
     def sum(self,x,y):
         print(x,y)
@@ -5,21 +6,38 @@ class Calc:
     def mul(self,x,y):
         print(x*y)
 
+    def __init__(self,name):
+         print(f'welcome {name}')
 
-class SciCalc:
-    def sum(self,x,y):
-        print(x+y)
 
-    def mul(self,x,y):
-        print(x*y)
+class SciCalc(Calc):
+
 
     def pow(self,x,y):
         print(x**y)
 
-s = SciCalc()
+s = SciCalc('jihad')
 s.sum(2,3)
 s.mul(4,5)
 s.pow(2,3)
+'''
+
+class A:
+    def do(self):
+        print('in A')
+
+class B(A):
+    pass
+
+class C:
+    def do(self):
+        print('in C')
+
+class D(B,C):
+    pass
+
+f = D()
+f.do()
 
 
 
